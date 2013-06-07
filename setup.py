1d01c405d1eec3b84f65a35e7681e761be0f2885
Jan Gosmann <jan@hyper-world.de>
2013-06-07 12:44:18 +0200

    Basic configuration data structures.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='fridge',
+    version='0.1',
+    description='Provenance tracking',  # TODO better description
+    author='Jan Gosmann',
+    author_email='jan@hyper-world.de',
+    # url= ... TODO
+    packages=find_packages())
