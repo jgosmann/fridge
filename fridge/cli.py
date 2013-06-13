be735afc38ed30a705697fdd8b293ec4189e101e
Jan Gosmann <jan@hyper-world.de>
2013-06-13 13:26:17 +0200

    Started CLI with init command.

---
+++ fridge/cli.py
@@ -0,0 +1,30 @@
+from fridge import Fridge
+import argparse
+import os
+import sys
+
+
+class FridgeCli(object):
+    def main(self, *args):
+        parser = argparse.ArgumentParser(
+            prog='fridge',
+            description='Fridge stores your scientific simulation results ' +
+            'and keeps them fresh.')
+        parser.add_argument(
+            'cmd', nargs=1, type=str, choices=self.dispatch.keys(),
+            help='Subcommand to execute.')
+        parser.add_argument('args', nargs=argparse.REMAINDER)
+        parsed = parser.parse_args(args)
+
+        self.dispatch[parsed.cmd[0]](self, *parsed.args)
+
+    def init(self, *args):
+        Fridge.init_dir(os.getcwd())
+
+    dispatch = {
+        'init': init
+    }
+
+
+if __name__ == '__main__':
+    FridgeCli().main(*sys.argv)
---
+++ fridge/test/test_cli.py
@@ -0,0 +1,25 @@
+from fridge.cli import FridgeCli
+from fridge import Fridge
+import os
+import shutil
+import tempfile
+
+
+class TestCli(object):
+    def setUp(self):
+        self.fridge_path = tempfile.mkdtemp()
+        self.orig_cwd = os.getcwd()
+        os.chdir(self.fridge_path)
+        self.cli = FridgeCli()
+
+    def tearDown(self):
+        os.chdir(self.orig_cwd)
+        shutil.rmtree(self.fridge_path)
+
+    def test_init_creates_new_fridge(self):
+        self.cli.main('init')
+        # check correct initialization by trying to open the fridge
+        Fridge(self.fridge_path)
+
+    # TODO test reinit/FridgeError
+    # TODO unknown subcommand
