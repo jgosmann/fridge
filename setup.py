b555f1563b5fd4675728f93d1351e1063d85fa26
Jan Gosmann <jgosmann@uwaterloo.ca>
2014-04-01 14:34:51 -0400

    Add basic setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name='fridge',
+    version='0.1',
+    author='Jan Gosmann',
+    author_email='jan@hyper-world.de',
+    packages=['fridge'],
+    provides=['fridge'],
+)
