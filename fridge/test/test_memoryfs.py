8353339f9a907767a6cb89d4e65497e7adb541d9
Jan Gosmann <jgosmann@uwaterloo.ca>
2014-04-01 14:38:19 -0400

    Add more tests for MemoryFile.

--- fridge/test/test_memoryfs.py
+++ fridge/test/test_memoryfs.py
@@ -7,3 +7,16 @@ class TestMemoryFile(object):
         f.write('test')
         f.flush()
         assert f.content == 'test'
+
+    def test_close_flushes_content(self):
+        f = MemoryFile()
+        f.write('test')
+        f.close()
+        assert f.content == 'test'
+
+    def test_can_be_reopened_and_read(self):
+        f = MemoryFile()
+        f.write('test')
+        f.close()
+        f.open()
+        assert f.read() == 'test'
