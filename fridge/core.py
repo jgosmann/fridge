a8673beffca9a03346d03ea0c6823c77ed138cd7
Jan Gosmann <jgosmann@uwaterloo.ca>
2014-04-03 13:17:32 -0400

    Implement writing/parsing snapshots in FridgeCore.

--- fridge/core.py
+++ fridge/core.py
@@ -1,10 +1,48 @@
 import os.path
 
 
+class SnapshotItem(object):
+    __slots__ = ['checksum', 'path']
+
+    def __init__(self, checksum, path):
+        self.checksum = checksum
+        self.path = path
+
+    def __eq__(self, other):
+        return self.checksum == other.checksum and self.path == other.path
+
+    # TODO test
+    @classmethod
+    def parse(cls, serialized):
+        # FIXME more robust splitting?
+        return cls(*serialized.strip().split(' '))
+
+
 class FridgeCore(object):
-    def __init__(self, fs, cas_factory):
+    def __init__(self, path, fs, cas_factory):
+        self._path = path
         self._fs = fs
-        self._blobs = cas_factory(os.path.join('.fridge', 'blobs'), fs)
+        self._blobs = cas_factory(os.path.join(path, '.fridge', 'blobs'), fs)
+        self._snapshots = cas_factory(os.path.join(
+            path, '.fridge', 'snapshots'), fs)
+
+    @classmethod
+    def init(cls, path, fs, cas_factory):
+        fs.mkdir(os.path.join(path, '.fridge'))
+        return cls(path, fs, cas_factory)
 
     def add_blob(self, path):
         self._blobs.store(path)
+
+    def add_snapshot(self, snapshot):
+        data = '\n'.join(
+            '{} {}'.format(item.checksum, item.path) for item in snapshot)
+        tmp_file = os.path.join(self._path, '.fridge', 'tmp')
+        with self._fs.open(tmp_file, 'w') as f:
+            f.write(data)
+        self._snapshots.store(tmp_file)
+
+    # TODO test this function
+    def parse_snapshot(self, serialized_snapshot):
+        return [SnapshotItem.parse(line)
+                for line in serialized_snapshot.split('\n')]
--- fridge/test/test_core.py
+++ fridge/test/test_core.py
@@ -1,10 +1,11 @@
+import hashlib
 import os.path
 
-from mock import MagicMock
+from mock import ANY, MagicMock
 import pytest
 
 from fridge.memoryfs import MemoryFS
-from fridge.core import FridgeCore
+from fridge.core import FridgeCore, SnapshotItem
 
 
 class CasMockFactory(object):
@@ -15,6 +16,16 @@ class CasMockFactory(object):
         path = os.path.relpath(path, '.fridge')
         if path not in self._cas:
             self._cas[path] = MagicMock()
+            self._cas[path].store.contents = []
+
+            def store(p):
+                try:
+                    with fs.open(p) as f:
+                        self._cas[path].store.contents.append(f.read())
+                except KeyError:  # FIXME should be OSError
+                    pass
+            self._cas[path].store.side_effect = store
+
         return self._cas[path]
 
     def __getitem__(self, key):
@@ -27,10 +38,28 @@ def cas_factory():
 
 @pytest.fixture
 def fridge(cas_factory):
-    return FridgeCore(MemoryFS(), cas_factory)
+    return FridgeCore.init('.', MemoryFS(), cas_factory)
 
 
 class TestFridgeCore(object):
+    def _create_snapshot(self):
+        # FIXME dont call sha1 directly.
+        return [
+            SnapshotItem(
+                checksum=hashlib.sha1('a').hexdigest(),
+                path='a'),
+            SnapshotItem(
+                checksum=hashlib.sha1('b').hexdigest(),
+                path='b')
+        ]
+
     def test_add_blob(self, cas_factory, fridge):
         fridge.add_blob('path')
         cas_factory['blobs'].store.assert_called_once_with('path')
+
+    def test_writing_and_parsing_snapshot(self, cas_factory, fridge):
+        s = self._create_snapshot()
+        fridge.add_snapshot(s)
+        cas_factory['snapshots'].store.assert_called_once_with(ANY)
+        assert s == fridge.parse_snapshot(
+            cas_factory['snapshots'].store.contents[0])
