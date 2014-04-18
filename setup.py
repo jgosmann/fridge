b412290cad1926e9a798caeb71c541b17fcd2a2a
Jan Gosmann <jan@hyper-world.de>
2014-04-18 17:36:41 -0400

    Fix setup.py given dirty test-output dir.

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+import os.path
+import shutil
 
+# The test output of the system tests may contain symlinks to non-existing
+# files which make distutils throw an exception (even though they are pruned).
+test_output_dir = os.path.join(os.curdir, 'systemtests', 'test-output')
+if os.path.exists(test_output_dir):
+    shutil.rmtree(test_output_dir)
 
 setup(
     name='fridge',
