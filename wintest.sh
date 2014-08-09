9fa56c0a9e16b277b1c4588746858616c74d188b
Jan Gosmann <jan@hyper-world.de>
2014-08-09 16:49:55 -0400

    Rsync before running win tests.

--- wintest.sh
+++ wintest.sh
@@ -6,4 +6,5 @@ VAGRANT_TOX='/cygdrive/c/Python27/Scripts/tox'
 VAGRANT_PYPATH='/cygdrive/c/Python27/:/cygdrive/c/Python33/:/cygdrive/c/Python34'
 
 vagrant up
+vagrant rsync
 vagrant ssh -c "cd /vagrant && PATH=$VAGRANT_PYPATH $VAGRANT_TOX"
